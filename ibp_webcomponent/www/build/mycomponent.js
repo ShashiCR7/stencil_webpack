28617b93ebcf4680fb0a141b244d534c76f4b599
DwivediShashikant <dwivedi.sk@outlook.com>
2018-11-12 15:39:32 +0530

    mutable false

--- ibp_webcomponent/www/build/mycomponent.js
+++ ibp_webcomponent/www/build/mycomponent.js
@@ -1,6 +1,6 @@
 /*!
  * Built with http://stenciljs.com
- * 2018-11-07T19:35:34
+ * 2018-11-07T19:37:26
  */
 (function(win, doc, namespace, fsNamespace, resourcesUrl, appCore, appCoreSsr, appCorePolyfilled, hydratedCssClass, components) {
 
